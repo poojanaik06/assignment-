261b01fdb69a6b522067c521db6472bb19a343c8
RahulGPi <crazynoania@outlook.com>
2025-12-10 19:48:22 +0530

    ball sac

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+// src/main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        {/* Route for the home page (Empty input) */}
+        <Route path="/" element={<App />} />
+        
+        {/* Route for specific keywords (e.g. localhost:5173/crypto) */}
+        <Route path="/:keyword" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
